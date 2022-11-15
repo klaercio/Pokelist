e8ebb2e63d2954a286b9df6ab531de13efd0e68f
Klaercio <klaerciosilva@gmail.com>
2022-11-15 18:24:34 -0300

    ❌Paginação removida e rolagem infinita adicionada

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Pokemon from "../../Components/Pokemon";
 import './index.css';
 
@@ -6,24 +6,43 @@ export default function Home() {
 
     const [pokemons, setPokemons] = useState([]);
     const[offset, setOffset] = useState(0);
-    const[limit, setLimit] = useState(20);
+    const[limit, setLimit] = useState(60);
+    const loaderRef = useRef(null);
 
     useEffect(()=> {
         fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}"`)
         .then(response => response.json())
-        .then(data => setPokemons(data.results));
+        .then(data => data.results.length? setPokemons(atual => [...atual, ...data.results]): console.log("nada mais a carregar")) 
     }, [offset, limit]);
 
+    useEffect(() => {
+        const options = {
+          root: null,
+          rootMargin: "20px",
+          threshold: 1.0
+        };
+    
+        const observer = new IntersectionObserver((entities) => {
+          const target = entities[0];
+    
+          if (target.isIntersecting){
+            setOffset(atual => atual+limit);
+          }
+        }, options);
+    
+        if (loaderRef.current){
+          observer.observe(loaderRef.current);
+        }
+      }, []);
 
     return <>
         <div className="Pokemons">
             <ul>
                 {pokemons.map((poke, index) => (
-                    <Pokemon name={poke.name} index={index}/>
+                    <Pokemon key={index} name={poke.name} index={index}/>
                 ))}
             </ul>
-            <button onClick={()=>setOffset(atual => atual+limit)}>Próxima</button>
-            <button onClick={()=>setOffset(atual => atual-limit)}>Anterior</button>
+            <p ref={loaderRef}/>
         </div>
     </>
 }
